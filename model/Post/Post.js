2091f68937593e9365bdeb2fc961d2d9609e952b
Muhammad Ahmed <ahmedaries.97@gmail.com>
2023-11-09 22:45:17 +0500

    Post, Comment, Category model

---
+++ model/Post/Post.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+// Schema
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    claps: {
+      type: Number,
+      default: 0,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    shares: {
+      type: Number,
+      default: 0,
+    },
+    postViews: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    schedulePublished: {
+      type: Date,
+      default: null,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//compile schema to model
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
